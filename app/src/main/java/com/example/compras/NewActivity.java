08fe47d057f0a83e01c4cf7ab4bb5957500ba0ef
Roberto Hernandez <hernandez.roberto014@gmail.com>
2019-03-29 19:58:49 -0600

    Boton Share

--- app/src/main/java/com/example/compras/NewActivity.java
+++ app/src/main/java/com/example/compras/NewActivity.java
@@ -3,6 +3,7 @@ package com.example.compras;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.compras.utils.AppConstants;
@@ -10,6 +11,8 @@ import com.example.compras.utils.AppConstants;
 public class NewActivity extends AppCompatActivity {
     TextView getUser, getEmail, totalProducto;
     TextView valorUno, valorDos, valorTres, valorCuatro, valorCinco, valorSeis, valorSiete, valorOcho, valorNueve;
+
+    Button compartir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +29,24 @@ public class NewActivity extends AppCompatActivity {
         valorOcho=findViewById(R.id.valor8);
         valorNueve=findViewById(R.id.valor9);
         totalProducto=findViewById(R.id.set_total);
+        compartir=findViewById(R.id.btn_share);
 
         getNumeroIntent();
+
+        Intent intentDos  = getIntent();
+        String[] texto = intentDos.getStringArrayExtra(Intent.EXTRA_TEXT);
+        /*if(intentDos!= null){
+            valorUno.setText(texto[0]);
+        }*/
+        compartir.setOnClickListener(v->{
+        Intent intentoTres = new Intent();
+        intentoTres.setAction(Intent.ACTION_SEND);
+        intentoTres.setType("text/plain");
+        intentoTres.putExtra(Intent.EXTRA_TEXT,texto[0]);
+
+        startActivity(intentoTres);
+        });
+
 
     }
     private void mostrando(){
@@ -38,9 +57,9 @@ public class NewActivity extends AppCompatActivity {
         var2 = bundle.getString(AppConstants.TEXT_KEY_TWO);
 
         getUser=(TextView) findViewById(R.id.set_user);
+        getEmail=(TextView) findViewById(R.id.set_email);
         getUser.setText(var1);
 
-        getEmail=(TextView) findViewById(R.id.set_email);
         getEmail.setText(var2);
     }
     public void getNumeroIntent(){
